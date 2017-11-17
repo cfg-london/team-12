eb9092554411a485caed3096f1b94ad4c33260d9
James Tavernor <jtavernor79@gmail.com>
2017-11-17 22:38:28 +0000

    website structure

---
+++ website/login.ini.php
@@ -0,0 +1,9 @@
+;<?php
+;die(); //#this code kills the script to ensure this code can never be printed
+;/*
+[database]
+username = admin
+hashedPassword = $2y$10$bVe5Ubc3WQcFc1AlSsjdZeBY4k/jQU3e2MeUCtmMdY8ihQ3aIGNQa
+#this file is kept outside root to prevent access by the web
+;*/
+;?>
