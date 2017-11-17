011e4d64987092f1b10732090c9d02e272d3e837
Nicholas Li <cnl16@ic.ac.uk>
2017-11-17 20:05:48 +0000

    Rebuilt project. #FirstEver

--- mobile_app/M/app/src/main/java/code_for_good/linkage/ProfileApp.java
+++ mobile_app/M/app/src/main/java/code_for_good/linkage/ProfileApp.java
@@ -3,11 +3,11 @@ package code_for_good.linkage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ProfileSetup extends AppCompatActivity {
+public class ProfileApp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_setup);
+        setContentView(R.layout.activity_profile_app);
     }
 }
