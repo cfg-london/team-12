f21f84a4c014f34f6a5f0431f64a32fdd4c0e195
Nicholas Li <cnl16@ic.ac.uk>
2017-11-17 22:40:37 +0000

    Making Userprofile a parcel

--- mobile_app/M/app/src/main/java/code_for_good/linkage/UserProfile.java
+++ mobile_app/M/app/src/main/java/code_for_good/linkage/UserProfile.java
@@ -1,22 +1,61 @@
 package code_for_good.linkage;
 
-import java.util.Optional;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-/**
- * Created by dimarammfire on 17.11.17.
- */
-
-class UserProfile {
+class UserProfile implements Parcelable {
 
     private String type;
-    private Optional<String> name;
-    private Optional<String> phone;
-    private Optional<String> email;
-    private Optional<String> address;
-    private Optional<String> workplace;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private String workplace;
 
-    public UserProfile(String type) {
+    public UserProfile(String type, String name, String phone, String email, String address, String workplace) {
         this.type = type;
-//        name.ifPresent(x -> this.name = x.);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.workplace = workplace;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public UserProfile(Parcel in){
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
     }
 }
