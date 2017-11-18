ea81523bc6ea6f1e229c14b480f17cad36663364
CX-16 <dimarammfire@CX-2.local>
2017-11-18 04:48:29 +0000

    [du216] Style correction for GPS.

--- mobile_app/M/app/src/main/java/code_for_good/linkage/GPSLocation.java
+++ mobile_app/M/app/src/main/java/code_for_good/linkage/GPSLocation.java
@@ -190,21 +190,16 @@ public class GPSLocation extends Service implements LocationListener {
     }
 
     @Override
-    public void onProviderDisabled(String provider) {
-    }
+    public void onProviderDisabled(String provider) {}
 
     @Override
-    public void onProviderEnabled(String provider) {
-    }
+    public void onProviderEnabled(String provider) {}
 
     @Override
-    public void onLocationChanged(android.location.Location location) {
-
-    }
+    public void onLocationChanged(android.location.Location location) {}
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-    }
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
 
     @Override
     public IBinder onBind(Intent arg0) {
