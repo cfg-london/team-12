1bf74baa44dd95f4978c0c6c015fefa4eb146dbf
Constantin Mueller <constantin.karl.mueller@gmail.com>
2017-11-18 03:34:25 +0000

    Issue selector functionality added

--- mobile_app/M/app/src/main/java/code_for_good/linkage/IssueSelector.java
+++ mobile_app/M/app/src/main/java/code_for_good/linkage/IssueSelector.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class IssueSelector extends AppCompatActivity {
 
@@ -17,6 +18,9 @@ public class IssueSelector extends AppCompatActivity {
     private Button buttonStayingInHome;
     private Button buttonMoney;
 
+    private EditText additional;
+    private Button confirm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +30,14 @@ public class IssueSelector extends AppCompatActivity {
         buttonSafetyAtHome = (Button) findViewById(R.id.buttonSafetyAtHome);
         buttonLegal = (Button) findViewById(R.id.buttonSafetyAtHome);
         buttonCrisis = (Button) findViewById(R.id.buttonCrisis);
-        buttonMedical = (Button) findViewById(R.id.buttonTransport);
-        buttonTransport = (Button) findViewById(R.id.buttonHelpAroundTheHouse);
+        buttonMedical = (Button) findViewById(R.id.buttonMedical);
+        buttonTransport = (Button) findViewById(R.id.buttonTransport);
+        buttonHelpAroundHouse = (Button) findViewById(R.id.buttonHelpAroundTheHouse);
         buttonStayingInHome = (Button) findViewById(R.id.buttonStayingInTheirHome);
         buttonMoney = (Button) findViewById(R.id.buttonMoneyAndBenefits);
+
+        additional = (EditText) findViewById(R.id.additional);
+        confirm = (Button) findViewById(R.id.confirm);
 
         final RequestBuilder requestBuilder = getIntent().getParcelableExtra("request");
 
@@ -37,6 +45,71 @@ public class IssueSelector extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 requestBuilder.toggleIssue("Isolation");
+            }
+        });
+
+        buttonSafetyAtHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.toggleIssue("Safety at home");
+            }
+        });
+
+        buttonLegal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.toggleIssue("Legal");
+            }
+        });
+
+        buttonCrisis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.toggleIssue("Crisis / Mental Issues");
+            }
+        });
+
+        buttonMedical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.toggleIssue("Medical Issue");
+            }
+        });
+
+        buttonTransport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.toggleIssue("Transport / Mobility Issues");
+            }
+        });
+
+        buttonStayingInHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.toggleIssue("Issues involving staying in their home");
+            }
+        });
+
+        buttonMoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.toggleIssue("Money / Benefits Issue");
+            }
+        });
+
+        buttonHelpAroundHouse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.toggleIssue("Requires help around the house");
+            }
+        });
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestBuilder.addIssue(additional.getText().toString());
+                Request request = requestBuilder.build();
+                // TODO: send
             }
         });
 
