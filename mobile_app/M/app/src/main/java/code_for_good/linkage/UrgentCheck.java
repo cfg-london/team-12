8d5711805cebd82d4caa3672533b7897bbdfe78f
Ash <ashley.see16@imperial.ac.uk>
2017-11-18 01:21:29 +0000

    Passed Lang to Profile App, and UrgentCheck Activities. TODO: add lang to each activity to carry forward, and list of strings in diff languages

--- mobile_app/M/app/src/main/java/code_for_good/linkage/UrgentCheck.java
+++ mobile_app/M/app/src/main/java/code_for_good/linkage/UrgentCheck.java
@@ -11,12 +11,14 @@ public class UrgentCheck extends AppCompatActivity {
     private Button urgent;
     private Button notUrgent;
     private Button volunteer;
+    private Languages lang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_urgent_check);
 
+        this.lang = (Languages) getIntent().getSerializableExtra("Language");
         urgent = (Button) findViewById(R.id.urgentCheckUrgent);
         notUrgent = (Button) findViewById(R.id.urgentCheckNotUrgent);
         volunteer = (Button) findViewById(R.id.urgentCheckVolunteer);
