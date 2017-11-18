7b89db03ae00f1473d2586138cce69e01300a1ce
Ash <ashley.see16@imperial.ac.uk>
2017-11-18 00:33:08 +0000

    Remove diff lang strings, not needed for that bit

--- mobile_app/M/app/src/main/java/code_for_good/linkage/ProfileApp.java
+++ mobile_app/M/app/src/main/java/code_for_good/linkage/ProfileApp.java
@@ -64,32 +64,7 @@ public class ProfileApp extends AppCompatActivity {
             }
         }
 
-        ArrayAdapter adapter;
-        switch(lang) {
-            case ENG:
-                adapter = ArrayAdapter.createFromResource(this, R.array.engReferrerOptions, R.layout.custom_spinner);
-                break;
-            case BNG:
-                adapter = ArrayAdapter.createFromResource(this, R.array.bngReferrerOptions, R.layout.custom_spinner);
-                break;
-            case CHI:
-                adapter = ArrayAdapter.createFromResource(this, R.array.chiReferrerOptions, R.layout.custom_spinner);
-                break;
-            case FRA:
-                adapter = ArrayAdapter.createFromResource(this, R.array.fraReferrerOptions, R.layout.custom_spinner);
-                break;
-            case ESP:
-                adapter = ArrayAdapter.createFromResource(this, R.array.espReferrerOptions, R.layout.custom_spinner);
-                break;
-            case ITA:
-                adapter = ArrayAdapter.createFromResource(this, R.array.itaReferrerOptions, R.layout.custom_spinner);
-                break;
-            case SOM:
-                adapter = ArrayAdapter.createFromResource(this, R.array.somReferrerOptions, R.layout.custom_spinner);
-                break;
-            default:
-                adapter = ArrayAdapter.createFromResource(this, R.array.engReferrerOptions, R.layout.custom_spinner);
-        }
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.engReferrerOptions, R.layout.custom_spinner);
 
         adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown);
         referrerDropdown = (Spinner) findViewById(R.id.professionDropdown);
