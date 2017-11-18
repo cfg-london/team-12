80110bce72b6cc36b21d0538cbed2dbf29f68af4
Nicholas Li <cnl16@ic.ac.uk>
2017-11-18 01:17:16 +0000

    request builder parcels

--- mobile_app/M/app/src/main/java/code_for_good/linkage/RequestBuilder.java
+++ mobile_app/M/app/src/main/java/code_for_good/linkage/RequestBuilder.java
@@ -1,21 +1,44 @@
 package code_for_good.linkage;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class RequestBuilder {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestBuilder implements Parcelable{
 
     private boolean urgent; // Is the request urgent?
     private UserProfile referrer; // Who is sending the referral?
-    private List<String> issues = new ArrayList<>(); // The list of issues that are the reason for the referral.
-    private Referee referree; // The information provided on the referee.
+    private List<String> issues; // The list of issues that are the reason for the referral.
+    private Referee referee; // The information provided on the referee.
 
-    private RequestBuilder() {}
+    private RequestBuilder() {
+        this.issues = new ArrayList<>();
+    }
+
+    public RequestBuilder(Parcel in) {
+        urgent = in.readByte() != 0;
+        referrer = in.readParcelable(UserProfile.class.getClassLoader());
+        in.readStringList(issues);
+        referee = in.readParcelable(Referee.class.getClassLoader());
+
+    }
+
+    public static final Creator<RequestBuilder> CREATOR = new Creator<RequestBuilder>() {
+        @Override
+        public RequestBuilder createFromParcel(Parcel in) {
+            return new RequestBuilder(in);
+        }
+
+        @Override
+        public RequestBuilder[] newArray(int size) {
+            return new RequestBuilder[size];
+        }
+    };
 
     public Request build() {
-        return new Request(urgent, referrer, issues, referree);
+        return new Request(urgent, referrer, issues, referee);
     }
 
     public RequestBuilder setUrgency(boolean urgent) {
@@ -38,9 +61,21 @@ public class RequestBuilder {
         return this;
     }
 
-    public RequestBuilder setReferree(Referee referree) {
-        this.referree = referree;
+    public RequestBuilder setReferee(Referee referee) {
+        this.referee = referee;
         return this;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeByte((byte) (urgent ? 1 : 0));
+        parcel.writeParcelable(referrer, i);
+        parcel.writeStringList(issues);
+        parcel.writeParcelable(referee, i);
+    }
 }
--- mobile_app/M/app/src/main/java/code_for_good/linkage/UrgentCheck.java
+++ mobile_app/M/app/src/main/java/code_for_good/linkage/UrgentCheck.java
@@ -4,48 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class UrgentCheck extends AppCompatActivity {
 
-
-    private Languages lang;
-    private boolean urgency;
+    private Button urgent;
+    private Button notUrgent;
+    private Button volunteer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_urgent_check);
 
-        this.lang = Languages.ENG;
-
-        if(getIntent().hasExtra("Language_Choice")){
-            String languages = getIntent().getStringExtra("Language_Choice");
-            switch (languages) {
-                case "English":
-                    lang = Languages.ENG;
-                    break;
-                case "বাঙালি":
-                    lang = Languages.BNG;
-                    break;
-                case "中文":
-                    lang = Languages.CHI;
-                    break;
-                case "Français":
-                    lang = Languages.FRA;
-                    break;
-                case "Español":
-                    lang = Languages.ESP;
-                    break;
-                case "Italiano":
-                    lang = Languages.ITA;
-                    break;
-                case "Somali":
-                    lang = Languages.SOM;
-                    break;
-                default:
-                    break;
-            }
-        }
+        urgent = (Button) findViewById(R.id.urgentCheckUrgent);
+        notUrgent = (Button) findViewById(R.id.urgentCheckNotUrgent);
+        volunteer = (Button) findViewById(R.id.urgentCheckVolunteer);
+        
     }
 
     public void urgent() {
